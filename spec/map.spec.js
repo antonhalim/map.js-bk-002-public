669dbf018df5fe292069dc06c8d456dbf563587e
Blake Johnson <blake41@gmail.com>
2014-05-21 15:32:01 -0400

    updated tests

--- spec/map.spec.js
+++ spec/map.spec.js
@@ -7,7 +7,11 @@ describe('map', function() {
     map = require('../lib/map');
   });
 
-  it('does a thing', function() {
+  it('can apply a function to each element', function() {
     expect(map([1,2,3], function(element) {element * element})).toEqual([1,4,9]);
+  });
+
+  it('can apply a function to each element', function() {
+    expect(map([16,12,3,45], function(element) {element - 1})).toEqual([15,11,2,44]);
   });
 });
