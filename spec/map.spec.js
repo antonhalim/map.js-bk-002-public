fade80c6957febf8533a0afc36448f04a85b4190
Katie Hoffman <ktahoffman@gmail.com>
2015-05-01 11:37:26 -0400

    add return to passed functions

--- spec/map.spec.js
+++ spec/map.spec.js
@@ -3,10 +3,11 @@
 describe('map', function() {
 
   it('can apply a function to each element', function() {
-    expect(map([1,2,3], function(element) {element * element})).toEqual([1,4,9]);
+    debugger;
+    expect(map([1,2,3], function(element) { return element * element})).toEqual([1,4,9]);
   });
 
   it('can apply a function to each element', function() {
-    expect(map([16,12,3,45], function(element) {element - 1})).toEqual([15,11,2,44]);
+    expect(map([16,12,3,45], function(element) { return element - 1})).toEqual([15,11,2,44]);
   });
 });
